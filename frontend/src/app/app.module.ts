fbc4322e07979f72826e96b501cffa153d9bd2ca
Hélio Fernandes <104803451+HelioAnacronista@users.noreply.github.com>
2022-10-14 21:20:24 -0300

    create method product

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -18,6 +18,7 @@ import { ProductCrudComponent } from './views/product-crud/product-crud.componen
 import { ProductCreateComponent } from './components/product/product-create/product-create.component';
 import { MatButtonModule } from "@angular/material/button";
 import { MatSnackBarModule } from "@angular/material/snack-bar";
+import { HttpClientModule } from "@angular/common/http";
 
 @NgModule({
   declarations: [
@@ -39,7 +40,8 @@ import { MatSnackBarModule } from "@angular/material/snack-bar";
     MatListModule,
     MatCardModule,
     MatButtonModule,
-    MatSnackBarModule
+    MatSnackBarModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
